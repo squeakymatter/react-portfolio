510515370cc87bf5ea066e7b3f955ca4d6f13174
Michelle Wolschlager <michelle.wolschlager@gmail.com>
2021-04-24 03:46:41 -0700

    add navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
 
 function App() {
-  return  (
-      <h2>test</h2>
-  );
+  return <Navbar />;
 }
 
 export default App;
