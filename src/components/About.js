2176b816fba629ebdfba1e0c908378c19e8f3cbe
Michelle Wolschlager <michelle.wolschlager@gmail.com>
2021-04-24 20:39:37 -0700

    about me container

---
+++ src/components/About.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import crest from '../assets/images/crest.svg';
+const About = () => {
+  return (
+    <div className='container py-5'>
+      <div className='row'>
+        <div className='col-lg-2 col-xm-12 about-photo mb-5'>
+          <img src={crest} alt='author' className='crest' />
+        </div>
+        <div className='col-lg-10 col-xm-12'>
+          <h1 className='content-header'>About Me</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim
+            commodi iste molestiae. Fuga nemo quas minus libero iure
+            praesentium, illum distinctio nam? Fuga nam nemo quasi porro fugit?
+            Rerum, laborum!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+            laudantium itaque quibusdam ab, aliquid, officia natus possimus
+            veritatis nemo voluptatibus mollitia iure quis? Tempora laboriosam
+            quas ea magni eum eligendi.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Typed from 'react-typed';
 
 const Header = () => {
     return (
         <div className="header-wrapper">
            <div className="main-info">
-           <h1> blah blah blah</h1>
+           <h1> Hello.</h1>
+           <Typed className="typed-text"
+           strings={["I am a Full Stack Developer."]}
+            typeSpeed={40}
+            backSpeed={60}
+            loop
+           />
+           <a href="#" className="btn-main-"></a>
            </div>
         </div>
     )
