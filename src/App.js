c6cbbebe02eafdbec8046e05035c034a3e401080
Michelle Wolschlager <michelle.wolschlager@gmail.com>
2021-04-24 23:53:49 -0700

    resume container

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import About from './components/About';
-
+import Resume from './components/Resume';
 function App() {
   return (
     <>
       <Navbar />
       <Header />
       <About />
+      <Resume />
     </>
   );
 }
---
+++ src/components/Resume.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Resume = () => {
+  return (
+    <div className='resume'>
+      <div className='d-flex justify-content-center my-5'>
+        <h1 className='content-header'>Resume</h1>
+      </div>
+      <div className='container resume-wrapper'>
+        <div className='timeline-block timeline-block-right'>
+          <div className='marker'></div>
+          <div className='timeline-content'>
+            <h4>Front-end</h4>
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>jQuery</li>
+              <li>React</li>
+              <li>Bootstrap</li>
+            </ul>
+          </div>
+        </div>
+        <div className='timeline-block timeline-block-left'>
+          <div className='marker'></div>
+          <div className='timeline-content'>
+            <h4>Back-end</h4>
+            <ul>
+              <li>Node</li>
+              <li>Express</li>
+              <li>MySQL | Sequelize</li>
+              <li>MongoDB | Mongoose</li>
+              <li>REST</li>
+              <li>APIs</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Resume;
